db8004ff22bcf8fab6529ef0b3a83b59087b31fd
amwatts1988 <alex@wattstech.pro>
2019-02-17 14:15:15 -0600

    added css for article page

--- js/custom.js
+++ js/custom.js
@@ -41,9 +41,7 @@
   $(window).scroll(function() {
     if ($(".navbar-default").offset().top > 50) {
       $(".navbar-fixed-top").addClass("top-nav-collapse");
-    } else {
-      $(".navbar-fixed-top").removeClass("top-nav-collapse");
-    }
+    } 
   });
 
   // Testimonials Slider
